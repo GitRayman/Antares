10b7edd0ad9f1db95ccf11e3ab5b04b872826b82
GitRayman <56263699+GitRayman@users.noreply.github.com>
2021-09-09 15:25:45 -0400

    Add files via upload

---
+++ Platform/DisplaySettings.h
@@ -0,0 +1,229 @@
+//User selectable configurations.
+//Define one if these in Platform.h
+//RJA Note:  These first two are for VGA output using a P2 pin in NCO mode to drive the clock of EVE to get correct output frequency, the others are for Matrix Orbital displays
+
+#ifdef EVE3_720p
+//VGA output using NCO clock to drive EVE
+#define DWIDTH      1280
+#define DHEIGHT     720
+#define PIXVOFFSET  0
+#define PIXHOFFSET  0
+#define HCYCLE      1650
+#define HOFFSET     260//Hsync+backporch 
+#define HSYNC0      0
+#define HSYNC1      40//hsync pulse width
+#define VCYCLE      750
+#define VOFFSET     25//vsync+backporch 
+#define VSYNC0      0
+#define VSYNC1      5//vsync pulse width 
+#define PCLK        1
+#define SWIZZLE     0
+#define PCLK_POL    0
+#define HSIZE       1280
+#define VSIZE       720
+#define CSPREAD     0
+#define DITHER      0
+#endif
+
+#ifdef EVE3_1080p
+//VGA output using NCO clock to drive EVE
+#define DWIDTH      1920
+#define DHEIGHT     1080
+#define PIXVOFFSET  0
+#define PIXHOFFSET  0
+#define HCYCLE      2200
+#define HOFFSET     192//Hsync+backporch 
+#define HSYNC0      0
+#define HSYNC1      44//hsync pulse width
+#define VCYCLE      1125
+#define VOFFSET     41//vsync+backporch 
+#define VSYNC0      0
+#define VSYNC1      5//vsync pulse width 
+#define PCLK        1
+#define SWIZZLE     0
+#define PCLK_POL    0
+#define HSIZE       1920
+#define VSIZE       1080
+#define CSPREAD     0
+#define DITHER      0
+#endif
+
+
+#ifdef EVE3_VGA  
+//VGA output using NCO clock to drive EVE
+#define DWIDTH      640L
+#define DHEIGHT     480L
+#define PIXVOFFSET  0
+#define PIXHOFFSET  0
+#define HCYCLE      800L
+#define HOFFSET     144L///Hsync+backporch 
+#define HSYNC0      0L //keep 0 //RJA
+#define HSYNC1      96L //hsync pulse width
+#define VCYCLE      525L
+#define VOFFSET     35L//vsync+backporch 
+#define VSYNC0      0L //keep 0//RJA 
+#define VSYNC1      2L //vsync pulse width 
+#define PCLK        3 //pixel clock divider
+#define SWIZZLE     0
+#define PCLK_POL    0//1  //either seems to work
+#define HSIZE       640
+#define VSIZE       480
+#define CSPREAD     0
+#define DITHER      0
+#endif
+
+
+
+#ifdef EVE2_70
+#define DWIDTH     800
+#define DHEIGHT    480
+#define PIXVOFFSET   0
+#define PIXHOFFSET   0
+#define HCYCLE     928
+#define HOFFSET     88
+#define HSYNC0       0
+#define HSYNC1      48
+#define VCYCLE     525
+#define VOFFSET     32
+#define VSYNC0       0
+#define VSYNC1       3
+#define PCLK         2
+#define SWIZZLE      0
+#define PCLK_POL     1
+#define HSIZE      800
+#define VSIZE      480
+#define CSPREAD      0
+#define DITHER       1
+#endif
+
+#ifdef EVE2_50
+#define DWIDTH     800
+#define DHEIGHT    480
+#define PIXVOFFSET   0
+#define PIXHOFFSET   0
+#define HCYCLE     928
+#define HOFFSET     88
+#define HSYNC0       0
+#define HSYNC1      48
+#define VCYCLE     525
+#define VOFFSET     32
+#define VSYNC0       0
+#define VSYNC1       3
+#define PCLK         2
+#define SWIZZLE      0
+#define PCLK_POL     1
+#define HSIZE      800
+#define VSIZE      480
+#define CSPREAD      0
+#define DITHER       1
+#endif
+
+#ifdef EVE2_43
+#define DWIDTH     480
+#define DHEIGHT    272
+#define PIXVOFFSET   0
+#define PIXHOFFSET   0
+#define HCYCLE     548
+#define HOFFSET     43
+#define HSYNC0       0
+#define HSYNC1      41
+#define VCYCLE     292
+#define VOFFSET     12
+#define VSYNC0       0
+#define VSYNC1      10
+#define PCLK         5
+#define SWIZZLE      0
+#define PCLK_POL     1
+#define HSIZE      480
+#define VSIZE      272
+#define CSPREAD      1
+#define DITHER       1
+#endif
+
+#ifdef EVE2_39
+#define DWIDTH     480
+#define DHEIGHT    128
+#define PIXVOFFSET   0
+#define PIXHOFFSET   0
+#define HCYCLE     524
+#define HOFFSET     16
+#define HSYNC0       0
+#define HSYNC1      44
+#define VCYCLE     288
+#define VOFFSET     12
+#define VSYNC0      11
+#define VSYNC1      8
+#define PCLK         5
+#define SWIZZLE      0
+#define PCLK_POL     1
+#define HSIZE      480
+#define VSIZE      128
+#define CSPREAD      1
+#define DITHER       1
+#endif
+  
+#ifdef EVE2_38
+#define DWIDTH     480
+#define DHEIGHT    116
+#define PIXVOFFSET  10
+#define PIXHOFFSET   0
+#define HCYCLE     524
+#define HOFFSET     43
+#define HSYNC0       0
+#define HSYNC1      41
+#define VCYCLE     292
+#define VOFFSET     12
+#define VSYNC0     152
+#define VSYNC1      10
+#define PCLK         5
+#define SWIZZLE      0
+#define PCLK_POL     1
+#define HSIZE      480
+#define VSIZE      272
+#define CSPREAD      1
+#define DITHER       1
+#endif
+
+#ifdef EVE2_35
+#define DWIDTH     320
+#define DHEIGHT    240
+#define PIXVOFFSET   0
+#define PIXHOFFSET   0
+#define HCYCLE     408
+#define HOFFSET     68
+#define HSYNC0       0
+#define HSYNC1      10
+#define VCYCLE     262
+#define VOFFSET     18
+#define VSYNC0       0
+#define VSYNC1       2
+#define PCLK         8
+#define SWIZZLE      0
+#define PCLK_POL     0
+#define HSIZE      320
+#define VSIZE      240
+#define CSPREAD      1
+#define DITHER       1
+#endif
+
+#ifdef EVE2_29
+#define DWIDTH     320
+#define DHEIGHT    102
+#define PIXVOFFSET   0
+#define PIXHOFFSET   0
+#define HCYCLE     408
+#define HOFFSET     70
+#define HSYNC0       0
+#define HSYNC1      10
+#define VCYCLE     262
+#define VOFFSET    156
+#define VSYNC0       0
+#define VSYNC1       2
+#define PCLK         8
+#define SWIZZLE      0
+#define PCLK_POL     0
+#define HSIZE      320
+#define VSIZE      102
+#define CSPREAD      1
+#define DITHER       1
+#endif
---
+++ Platform/Platform.h
@@ -0,0 +1,148 @@
+
+#ifndef _PLATFORM_H_
+#define _PLATFORM_H_
+
+
+
+
+//Antares uses a P2 smartpin in NCO mode to give clock to EVE3 chip in order to get TV and monitors to accept VGA/720p signals selected by the below
+//Note:  Eve sometimes gets stuck when switched from VGA to 720p
+//Pick one: (see more options in DisplaySettings.h)
+#define EVE3_720p  
+//#define EVE3_VGA  
+//#define EVE2_70  //7" WVGA LCD
+//#define EVE2_43  //4.3" LCD
+
+//Note:  For VGA, there other frequency options, but for 720p we need 297 MHz
+#ifdef EVE3_VGA
+enum { _clkfreq = 200000000 }; //Note: If you change this you must also change the NCO values to maintain 12 MHz output
+#elif defined EVE3_720p
+enum { _clkfreq = 297000000 }; 
+#elif defined EVE2_70
+enum { _clkfreq = 160000000 };
+#elif defined EVE3_1080p
+enum { _clkfreq = 237600000 };  //24 Hz
+#endif
+
+
+enum { heapsize = 16000 };  //override the default heapsize to give more, if needed
+
+//Pick one of these board configs or add your own
+#define AntaresB
+//#define Board1
+//#define Dazzler
+
+//P2 Eval board with AntaresB 
+#ifdef AntaresB
+#define basepin (0) //This is the starting pin on which the Antares-B adapter is situated
+//Using this P2 pin to supply ~12 MHz clock to eve chip
+#define NCOPIN (basepin+2)
+
+#ifdef EVE3_VGA
+#define NCOYPIN $4000_0000  //wypin value for NCO oscillator
+#define NCOXPIN 4
+#else
+#define NCOYPIN $8000_0000  //wypin value for NCO oscillator
+#define NCOXPIN 12
+#endif
+
+#define FT800_CS                                (basepin+5)
+#define FT800_INT                               (basepin+7) 
+#define FT800_PD_N                              (basepin+6)
+#define FT800_SEL_PIN           FT800_CS
+//RJA adding more pins
+#define FT800_MOSI								(basepin+0)
+#define FT800_MISO								(basepin+1)
+#define FT800_CLK								(basepin+4)
+#endif
+
+//RJA's P2 Board1 with EVE2 display
+#ifdef Board1
+#define FT800_CS                                (30)
+#define FT800_INT                               (28)
+#define FT800_PD_N                              (31)
+#define FT800_SEL_PIN           FT800_CS
+#define FT800_MOSI								(24)
+#define FT800_MISO								(25)
+#define FT800_CLK								(29)
+#endif
+
+//RJA's Arduino style P2 Board with Dazzler
+#ifdef Dazzler
+#define FT800_CS                                (50)
+#define FT800_INT                               (36)
+#define FT800_PD_N                              (-1)
+#define FT800_SEL_PIN           FT800_CS
+#define FT800_MOSI								(53)
+#define FT800_MISO								(54)
+#define FT800_CLK								(55)
+#define Dazzler_CS								(52)
+#define DazzlerSD_CS								(51)
+#endif
+
+
+//Pick one or none of these to say where files might come from
+#define USE_SD
+//#define USE_HOST
+
+//USB pin definitions for AntaresB
+#define USB_BASE_PIN 16 //(basepin+3)  //use AntaresB onboard LED for USB activity
+#define USB_ENABLE_PIN -1  //not using a P2 pin to enable AntaresB USB
+#define USB_DM_PIN (basepin+14)  //USB is on +14 and +15
+#define USB_ERR_LED_PIN 57       //Using P2 Eval board's P57 led for error display
+
+////USB pin definitions Parallax USB adapter board for P2 Eval Board
+//#define SERIAL_HOST_BASEPIN 16
+//#define USB_BASE_PIN (SERIAL_HOST_BASEPIN+0) 
+//#define USB_ENABLE_PIN (SERIAL_HOST_BASEPIN+1)  
+//#define USB_DM_PIN (SERIAL_HOST_BASEPIN+6)  //USB is on +6 and +7 for lower port, +2 and +3 for upper port
+//#define USB_ERR_LED_PIN 57       //Using P2 Eval board's P57 led for error display
+
+
+
+///* Threshold for resistance */
+#define RESISTANCE_THRESHOLD                    (1200) //RJA:  for resistive touch panels
+
+
+
+
+//typedef boolean bool_t;
+#define TRUE     (1)
+#define FALSE    (0)
+
+// override baud rate for serial, requires use of "_setbaud(_BAUD);" in code
+#ifndef _BAUD
+#define _BAUD 2000000
+#endif
+
+//Add in some needed includes here
+#include "propeller2.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdint.h"
+#include "stdbool.h"
+#include "DisplaySettings.h" //Settings for various types of displays are here, 
+
+
+//some types not yet defined in FlexSpin, or needed to translate Arduino code
+typedef char  byte;
+typedef unsigned char uchar;
+typedef char		bool_t;
+typedef char		char8_t;
+typedef unsigned char	uchar8_t;
+typedef unsigned char prog_uchar8_t;
+typedef signed char	schar8_t;
+typedef float		float_t;
+
+//RJA:  Seem to need this...
+#define min(x,y)  ((x) > (y) ? (y) : (x))
+#define max(x,y)  ((x) > (y) ? (x) : (y))
+
+
+
+#endif /*_PLATFORM_H_*/
+/* Nothing beyond this*/
+
+
+
+
